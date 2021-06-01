456add9e08cf4b30064c8c003c8eb09caa085e06
w4rlock <warlock.gpl@gmail.com>
2021-06-01 17:15:23 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,6 +1,39 @@
-module.exports.hello = async() => {
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const Ssm = new AWS.SecretsManager({ // eslint-disable-line
+  endpoint: 'https://secretsmanager.us-east-1.amazonaws.com',
+  region: 'us-east-1'
+});
+
+const SecretId = '/path/to/NEW_RELIC_LICENSE_KEY';
+
+
+const testTraceDistribuidFeature = async() => {
+  const data = await Ssm.getSecretValue({ SecretId }).promise();
+  if (data && data.SecretString) {
+    console.log('Secret length: ' + data.SecretString.length);
+  }
+
+  await s3.listBuckets({}).promise();
+
+  return true;
+}
+
+module.exports.hello = async(event) => {
+  await testTraceDistribuidFeature();
+
   const body = { foo: 'bar' };
-  const responseCode = 200;
+  const responseCode = event && event.responseCode
+    ? event.responseCode
+    : 200;
+
+  // TODO: para probar los casos de errores de la lambda
+  // En la invocacion le pasamos un obj como evento Ejemplo:
+  // { "responseCode": 502, "err": "test undefined error" }
+  if (responseCode === 502) {
+    const err = event.err ? event.err : 'testttt message errorrrr';
+    throw new Error(err);
+  }
 
   const response = {
     statusCode: responseCode,
@@ -11,7 +44,6 @@ module.exports.hello = async() => {
     body: JSON.stringify(body)
   };
 
-  console.log(process.env);
   console.log("response: " + JSON.stringify(response));
   return response;
 };
